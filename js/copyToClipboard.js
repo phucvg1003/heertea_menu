803b72543fd4827f795ef5d0d9996f14ee4f6a3e
phucvg1003 <phucvg1003@gmail.com>
2023-07-26 10:25:45 +0700

    Update Js and index

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -2,10 +2,8 @@
 const checkboxes = document.querySelectorAll("input[type=checkbox]");
 const selectedDishes = document.getElementById("selectedDishes");
 
-
-// Add an event listener to the copy button.
-copyButton.addEventListener("click", () => {
-    const x = document.getElementById("showHideModal");
+function copyClipboard() {
+  const x = document.getElementById("showHideModal");
 
     const s = document.getElementById("success_mess");
     const f = document.getElementById("fail_mess");
@@ -40,7 +38,43 @@ copyButton.addEventListener("click", () => {
     // Copy the array to the clipboard.
     navigator.clipboard.writeText(values.join(", "+"\n\n"));
     selectedDishes.innerHTML = values.join("<br>");
-});
+}
 
+// Add an event listener to the copy button.
+// copyButton.addEventListener("click", () => {
+//     const x = document.getElementById("showHideModal");
 
+//     const s = document.getElementById("success_mess");
+//     const f = document.getElementById("fail_mess");
 
+//     if (x.style.display === "none") {
+//         x.style.display = "none";
+//       } else {
+//         x.style.display = "block";
+//       }
+
+//     // Loop through the checkboxes and copy their values to an array.
+//     const values = [];
+
+//     for (let checkbox of checkboxes) {
+//         if (checkbox.checked === true) {
+//             values.push(checkbox.value);
+//             if (s.style.display === "none") {
+//                 s.style.display = "none";
+//               } else {
+//                 s.style.display = "block";
+//                 f.style.display = "none";
+//               }
+//         } else {
+//             if (f.style.display === "none") {
+//                 f.style.display = "none";
+//               } else {
+//                 f.style.display = "block";
+//               }
+//         }
+//     } 
+
+//     // Copy the array to the clipboard.
+//     navigator.clipboard.writeText(values.join(", "+"\n\n"));
+//     selectedDishes.innerHTML = values.join("<br>");
+// });
